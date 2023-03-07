036909c8a5ad11a403cb2c3658713b90148ad1a3
wodahS21 <yaelelyolo@gmail.com>
2023-03-07 13:41:04 -0700

    Agregar archivos de proyecto.

---
+++ Examen/Form1.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace Examen
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pbPapel = new System.Windows.Forms.PictureBox();
+            this.btnImagen = new System.Windows.Forms.Button();
+            this.btnGuardar = new System.Windows.Forms.Button();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.toolStripButton1 = new System.Windows.Forms.ToolStripButton();
+            ((System.ComponentModel.ISupportInitialize)(this.pbPapel)).BeginInit();
+            this.toolStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pbPapel
+            // 
+            this.pbPapel.BackColor = System.Drawing.Color.White;
+            this.pbPapel.Location = new System.Drawing.Point(93, 12);
+            this.pbPapel.Name = "pbPapel";
+            this.pbPapel.Size = new System.Drawing.Size(825, 482);
+            this.pbPapel.TabIndex = 0;
+            this.pbPapel.TabStop = false;
+            this.pbPapel.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pbPapel_MouseDown);
+            this.pbPapel.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pbPapel_MouseMove);
+            this.pbPapel.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pbPapel_MouseUp);
+            // 
+            // btnImagen
+            // 
+            this.btnImagen.Location = new System.Drawing.Point(12, 244);
+            this.btnImagen.Name = "btnImagen";
+            this.btnImagen.Size = new System.Drawing.Size(75, 23);
+            this.btnImagen.TabIndex = 1;
+            this.btnImagen.Text = "Agregar";
+            this.btnImagen.UseVisualStyleBackColor = true;
+            this.btnImagen.Click += new System.EventHandler(this.btnImagen_Click);
+            // 
+            // btnGuardar
+            // 
+            this.btnGuardar.Location = new System.Drawing.Point(14, 218);
+            this.btnGuardar.Name = "btnGuardar";
+            this.btnGuardar.Size = new System.Drawing.Size(75, 23);
+            this.btnGuardar.TabIndex = 2;
+            this.btnGuardar.Text = "Guardar";
+            this.btnGuardar.UseVisualStyleBackColor = true;
+            this.btnGuardar.Click += new System.EventHandler(this.btnGuardar_Click);
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripButton1});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(909, 25);
+            this.toolStrip1.TabIndex = 3;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // toolStripButton1
+            // 
+            this.toolStripButton1.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.toolStripButton1.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton1.Image")));
+            this.toolStripButton1.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton1.Name = "toolStripButton1";
+            this.toolStripButton1.Size = new System.Drawing.Size(23, 22);
+            this.toolStripButton1.Text = "toolStripButton1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(909, 506);
+            this.Controls.Add(this.toolStrip1);
+            this.Controls.Add(this.btnGuardar);
+            this.Controls.Add(this.btnImagen);
+            this.Controls.Add(this.pbPapel);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pbPapel)).EndInit();
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private PictureBox pbPapel;
+        private Button btnImagen;
+        private Button btnGuardar;
+        private ToolStrip toolStrip1;
+        private ToolStripButton toolStripButton1;
+    }
+}
---
+++ Examen/Form1.cs
@@ -0,0 +1,90 @@
+using Microsoft.VisualBasic.Logging;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Windows.Forms;
+
+namespace Examen
+{
+    public partial class Form1 : Form
+    {
+        Graphics papel;
+        bool paint;
+        private Graphics g;
+        private Point? previousPoint;
+        private Pen pen = new Pen(Color.Black, 3);
+        public Form1()
+        {
+            InitializeComponent();
+
+
+            pbPapel.Image = new Bitmap(pbPapel.Height, pbPapel.Width);
+            papel = pbPapel.CreateGraphics();
+            pen = new Pen(Color.FromArgb(0, 0, 0), 3);
+            papel.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias;
+        }
+
+        private void btnImagen_Click(object sender, EventArgs e)
+
+        {
+
+
+            OpenFileDialog getimage = new OpenFileDialog();
+            DialogResult result = getimage.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                pbPapel.Image = Image.FromFile(getimage.FileName);
+
+            }
+
+        }
+
+
+
+        private void pbPapel_MouseDown(object sender, MouseEventArgs e)
+        {
+            // Inicializa la variable Graphics a partir del PictureBox
+            g = pbPapel.CreateGraphics();
+           
+        }
+
+        private void pbPapel_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left && g != null) // Verifica si se está presionando el botón izquierdo del mouse y si ya se inicializó el objeto Graphics
+            {
+                // Dibuja un círculo en la posición del mouse
+                g.FillEllipse(Brushes.Black, e.X - 10, e.Y - 10, 10, 10);
+            }
+            //  previousPoint = e.Location;
+        }
+
+        private void pbPapel_MouseUp(object sender, MouseEventArgs e)
+        {
+            pbPapel.Show();
+            g.Dispose();
+            g = null;
+            
+
+            //previousPoint = null;
+        }
+
+        private void btnGuardar_Click(object sender, EventArgs e)
+        {
+             
+            Bitmap bmp = new Bitmap(pbPapel.Width, pbPapel.Height);
+            pbPapel.DrawToBitmap(bmp, new Rectangle(0, 0, pbPapel.Width, pbPapel.Height));
+
+            // Define el nombre del archivo y la ruta donde se guardará la imagen
+            string fileName = "dibujo.png";
+            string filePath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Desktop), fileName);
+
+            // Guarda el dibujo en el disco duro
+            bmp.Save(filePath, ImageFormat.Png);
+
+            // Libera los recursos del objeto Bitmap
+            bmp.Dispose();
+
+        }
+
+
+}
+}
